73506901560347c99345a9f8913acf8831c3dd09
Salavat F <salmrproperfahr@mail.ru>
2022-08-15 15:28:30 +0300

    styled table

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -3,6 +3,7 @@ import { useTypedSelector } from "../../hooks/useTypedSelector";
 import SearchForm from "../SearchForm/SearchForm";
 import PhoneList from "../PhoneList/PhoneList";
 import Header from "../Header/Header";
+import AddForm from "../AddForm/AddForm";
 
 const MainPage: React.FC = () => {
     const { user } = useTypedSelector((state) => state.user);
@@ -11,6 +12,7 @@ const MainPage: React.FC = () => {
         return (
             <div className="container">
                 <Header />
+                <AddForm />
                 <SearchForm />
                 <PhoneList />
             </div>
